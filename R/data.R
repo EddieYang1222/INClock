25faea63d6ce8a91aa36e1638c7e6813f121140f
Eddie Yang <yang1222@wharton.upenn.edu>
2024-09-01 00:00:14 -0400

    Updated data.R

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Tabula Muris Senis (TMS) single-cell RNA-seq dataset
+#'
+#' 20,138 genes and 40,220 cells from mouse bone marrow.
+#'
+#' @references Tabula Muris Consortium (2020). A single-cell transcriptomic
+#' atlas characterizes ageing tissues in the mouse. \emph{Nature, 583}(7817),
+#' 590–595.
+#'
+"tms_marrow"
